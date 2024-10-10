a596967b44ec7d16c35a7e89ed3217bf0ac589fb
Ankit <ankitumredkar77@gmail.com>
2024-10-10 18:35:49 +0530

    Initial commit

---
+++ lib/view/home_page.dart
@@ -0,0 +1,65 @@
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  //build a separate function to check connectivity
+  Future<void> checkConnectivity(BuildContext context) async {
+    var connectivityResult = await Connectivity().checkConnectivity();
+
+    if (connectivityResult.contains(ConnectivityResult.wifi) ||
+        connectivityResult.contains(ConnectivityResult.mobile)) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'You are Online',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.green,
+        ),
+      );
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+            'No internet connection!',
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.red,
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.indigo,
+        centerTitle: true,
+        title: const Text(
+          'Connectivity Plus',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            checkConnectivity(context);
+          },
+          child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.indigo,
+              ),
+              padding: const EdgeInsets.all(13),
+              child: const Text(
+                'Check Connectivity',
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
+      ),
+    );
+  }
+}
